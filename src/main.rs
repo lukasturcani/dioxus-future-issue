3f57f42b4a393236dd2fca9821a550e9ecb36e55
Lukas Turcani <lukasturcani93@gmail.com>
2023-12-11 11:46:17 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use dioxus::prelude::*;
+
+fn main() {
+    wasm_logger::init(wasm_logger::Config::default());
+    dioxus_web::launch(App)
+}
+
+enum Page {
+    First,
+    Second,
+}
+
+#[component]
+fn App(cx: Scope) -> Element {
+    use_shared_state_provider(cx, || Page::First);
+    let page = use_shared_state::<Page>(cx).unwrap();
+    cx.render(rsx! {
+        match *page.read() {
+            Page::First => rsx! {
+                First {}
+            },
+            Page::Second => rsx! {
+                div {
+                    "Second page"
+                }
+            }
+        }
+    })
+}
+
+#[component]
+fn First(cx: Scope) -> Element {
+    let page = use_shared_state::<Page>(cx).unwrap();
+    cx.render(rsx! {
+        div {
+            "First page"
+            button {
+                onclick: |_| {
+                    *page.write() = Page::Second;
+                    // cx.spawn(do_async_stuff());
+                    // cx.spawn_forever(do_async_stuff());
+                    do_async_stuff()
+                },
+                "Go to second page"
+            }
+        }
+    })
+}
+
+async fn do_async_stuff() {
+    log::info!("Doing async stuff");
+    async_std::task::sleep(std::time::Duration::from_secs(1)).await;
+    log::info!("Done doing async stuff");
+}
